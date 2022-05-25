e57ae43c1d45e3322fb531a6b602ce7d737bfdb0
ohyoungkyu <come8908@gmail.com>
2022-05-25 20:43:09 +0900

    할일리스트 API 구현

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 import express from "express";
 import cors from "cors";
+import mysql from "mysql2/promise";
+
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "come8900",
+  password: "dudrb2682!",
+  database: "todoapp_2022_05_25",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  dateStrings: true,
+});
 
 const app = express();
 
@@ -13,15 +25,24 @@ app.use(express.json());
 
 const appPort = 3000;
 
-app.get(`/todos`, (req, res) => {
-  return res.json([
-    {
-      id: 2,
-    },
-    {
-      id: 1,
-    },
-  ]);
+app.get(`/:user_code/todos`, async (req, res) => {
+  const { user_code } = req.params;
+
+  const [todoRows] = await pool.query(
+    `
+  SELECT * 
+  FROM todo 
+  WHERE user_code = ?
+  ORDER BY id DESC
+  `,
+    [user_code]
+  );
+
+  res.json({
+    resultCode: "S-1",
+    msg: "성공",
+    data: todoRows,
+  });
 });
 
 app.listen(appPort, () => {
